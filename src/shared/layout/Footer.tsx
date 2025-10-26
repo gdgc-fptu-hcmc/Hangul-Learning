90b7e43ca053755a4bc83d20e7c8c4e1871f9246
Mai Nguyen Duy Khanh <mndkhanh@gmail.com>
2025-10-26 19:22:34 +0700

    footer reponsize

--- src/shared/layout/Footer.tsx
+++ src/shared/layout/Footer.tsx
@@ -11,15 +11,16 @@ const Footer: React.FC<FooterProps> = ({ className = "" }) => {
     <footer
       className={`w-full bg-white border-t-[1px] border-gray-300 ${className}`}
     >
-      <div className="md:w-[95vw] mx-auto h-[45px] flex items-center justify-between">
+      <div className="w-[95%] md:w-[95vw] mx-auto py-4 md:py-2 flex flex-col md:flex-row items-center justify-between gap-4 md:gap-0">
         {/* ===== Left: Logo + Copyright ===== */}
-        <div className="flex items-center gap-6">
+        <div className="flex flex-row items-center gap-4 md:gap-6">
           {/* Logo */}
           <ScrollToTop>
             <div className="w-[100px] hover:cursor-pointer">
               <img src={logo} alt="Logo" className="w-full h-auto" />
             </div>
           </ScrollToTop>
+
           {/* Copyright */}
           <div className="flex items-center gap-2 text-gray-700">
             <div className="flex items-center justify-center w-5 h-5 border-2 border-gray-700 rounded-full">
@@ -30,17 +31,18 @@ const Footer: React.FC<FooterProps> = ({ className = "" }) => {
             </p>
           </div>
         </div>
+
         {/* ===== Right: Links ===== */}
-        <div className="flex items-center gap-12 text-base font-semibold text-gray-700">
+        <div className="flex flex-col md:flex-row items-center gap-2 md:gap-12 text-base font-semibold text-gray-700">
           <a
             href="#terms"
-            className="hover:text-gray-500 transition-colors whitespace-nowrap"
+            className="hover:text-gray-500 transition-colors whitespace-nowrap text-center md:text-left"
           >
             Điều khoản sử dụng
           </a>
           <a
             href="#privacy"
-            className="hover:text-gray-500 transition-colors whitespace-nowrap"
+            className="hover:text-gray-500 transition-colors whitespace-nowrap text-center md:text-left"
           >
             Chính sách quyền riêng tư
           </a>
