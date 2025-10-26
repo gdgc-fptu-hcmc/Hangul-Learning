462b4059dd01c277e01fc8cda52716247e046291
Mai Nguyen Duy Khanh <mndkhanh@gmail.com>
2025-10-26 10:45:38 +0700

    file structure organize

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import LearningPage from './components/LearningPage';
-import './App.css';
+import LearningPage from "./components/LearningPage";
+import "./App.css";
+import HeaderNav from "./shared/layout/HeaderNav";
+import NavBar from "./shared/layout/NavBar";
+import Footer from "./shared/layout/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <LearningPage />
-    </div>
+    <>
+      <Footer></Footer>
+    </>
   );
 }
 
