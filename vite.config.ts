1d8ea16d55db2abe798ad7b98b46769c1bb2bacc
TRAN MINH KHANG <minhkhang.fptu@gmail.com>
2025-11-04 21:57:29 +0700

    Revert "Page Bai_Hoc_So_Cap"

--- vite.config.ts
+++ vite.config.ts
@@ -1,45 +1,20 @@
-import { defineConfig, Plugin } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { createServer } from "./server";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { fileURLToPath, URL } from 'node:url';
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  root: 'client', // <--- Dòng này vẫn giữ nguyên
-  server: {
-    host: "::",
-    port: 8080,
-    fs: {
-    
-      allow: [
-        path.resolve(__dirname, "./client"),
-        path.resolve(__dirname, "./shared")
-      ],
-      deny: [".env", ".env.*", "*.{crt,pem}", "**/.git/**", "server/**"],
-    },
-  },
-  build: {
-    outDir: "dist/spa",
-  },
-  plugins: [react(), expressPlugin()],
+export default defineConfig({
+  plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./client"),
-      "@shared": path.resolve(__dirname, "./shared"),
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-}));
+  server: {
+    port: 5173
+  },
+  preview: {
+    port: 5173
+  },
+});
 
-function expressPlugin(): Plugin {
-  return {
-    name: "express-plugin",
-    apply: "serve", 
-    configureServer(server) {
-      const app = createServer();
-
-      
-      server.middlewares.use(app);
-    },
-  };
-}
 
