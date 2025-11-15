6f7363c9d82ed62de0383c2b422247c1d2758421
Mai Nguyen Duy Khanh <mndkhanh@gmail.com>
2025-11-16 01:32:14 +0700

    done all roadmap

--- src/components/courses-topics/topic/TopicSection.tsx
+++ src/components/courses-topics/topic/TopicSection.tsx
@@ -13,7 +13,8 @@ const TopicSection: React.FC<TopicSectionProps> = ({
   className = "",
 }) => {
   if (!learningTopics.length) return null;
-
+  console.log("Rendering TopicSection with topics:", learningTopics);
+  console.log(learningTopics[0].lessons[0].icon);
   return (
     <section className={`w-full flex justify-center ${className}`}>
       <div className="w-full flex flex-col gap-12">
@@ -21,7 +22,7 @@ const TopicSection: React.FC<TopicSectionProps> = ({
           <div key={topic.id} className="flex flex-col gap-4">
             {/* TopicTitle Component */}
             <TopicTitle
-              topicNumber={index + 1}
+              topicNumber={topic.id}
               title={topic.title}
               topicTitleClassName={topic.topicTitleClassName}
             />
@@ -43,60 +44,63 @@ const TopicSection: React.FC<TopicSectionProps> = ({
               "
             >
               {/* road map dashed line */}
-              <div className="max-w-xl h-full mx-auto  grid grid-cols-2 auto-rows-[150px]">
-                <div className="relative border-t-4 border-l-4 border-dashed border-gray-300 rounded-tl-[100%]">
-                  <LessonNodeOnRoadmap
-                    icon={topic.lessons[0].icon}
-                    title={topic.lessons[0].title}
-                    lessonLink={topic.lessons[0].lessonLink}
-                    miniGameLink={topic.lessons[0].miniGameLink}
-                    mainColor={topic.mainColor}
-                    className="absolute left-full -translate-x-1/2 top-0 -translate-y-1/2 z-[999]"
-                  />
+              {topic.lessons && topic.lessons.length > 0 && (
+                <div className="max-w-xl h-full mx-auto  grid grid-cols-2 auto-rows-[150px]">
+                  <div className="relative border-t-4 border-l-4 border-dashed border-gray-300 rounded-tl-[100%]">
+                    <LessonNodeOnRoadmap
+                      icon={topic.lessons[0].icon}
+                      title={topic.lessons[0].title}
+                      lessonLink={topic.lessons[0].lessonLink}
+                      miniGameLink={topic.lessons[0].miniGameLink}
+                      mainColor={topic.mainColor}
+                      className="absolute left-full -translate-x-1/2 top-0 -translate-y-1/2 z-[999]"
+                    />
+                  </div>
+                  <div className=""></div>
+                  <div className="relative border-b-4 border-l-4 border-dashed border-gray-300 rounded-bl-[100%]">
+                    <LessonNodeOnRoadmap
+                      icon={topic.lessons[1].icon}
+                      title={topic.lessons[1].title}
+                      lessonLink={topic.lessons[1].lessonLink}
+                      miniGameLink={topic.lessons[1].miniGameLink}
+                      mainColor={topic.mainColor}
+                      className="absolute left-0 -translate-x-1/2 top-0 -translate-y-1/2  z-[998]"
+                    />
+                  </div>
+                  <div className=""></div>
+                  <div className=""></div>
+                  <div className="relative border-t-4 border-r-4 border-dashed border-gray-300  rounded-tr-[100%]">
+                    <LessonNodeOnRoadmap
+                      icon={topic.lessons[2].icon}
+                      title={topic.lessons[2].title}
+                      lessonLink={topic.lessons[2].lessonLink}
+                      miniGameLink={topic.lessons[2].miniGameLink}
+                      mainColor={topic.mainColor}
+                      className="absolute left-0 -translate-x-1/2 top-0 -translate-y-1/2 z-[997]"
+                    />
+                  </div>
+                  <div className=""></div>
+                  <div className="relative border-b-4 border-r-4 border-dashed border-gray-300  rounded-br-[100%]">
+                    <LessonNodeOnRoadmap
+                      icon={topic.lessons[3].icon}
+                      title={topic.lessons[3].title}
+                      lessonLink={topic.lessons[3].lessonLink}
+                      miniGameLink={topic.lessons[3].miniGameLink}
+                      mainColor={topic.mainColor}
+                      className="absolute left-full -translate-x-1/2  top-0 -translate-y-1/2 z-[996]"
+                    />
+                    <LessonNodeOnRoadmap
+                      icon={topic.lessons[4].icon}
+                      title={topic.lessons[4].title}
+                      lessonLink={topic.lessons[4].lessonLink}
+                      miniGameLink={topic.lessons[4].miniGameLink}
+                      mainColor={topic.mainColor}
+                      className="absolute right-0 -translate-x-1/2  bottom-0 translate-y-1/2 z-[995]"
+                    />
+                  </div>
                 </div>
-                <div className=""></div>
-                <div className="relative border-b-4 border-l-4 border-dashed border-gray-300 rounded-bl-[100%]">
-                  <LessonNodeOnRoadmap
-                    icon={topic.lessons[1].icon}
-                    title={topic.lessons[1].title}
-                    lessonLink={topic.lessons[1].lessonLink}
-                    miniGameLink={topic.lessons[1].miniGameLink}
-                    mainColor={topic.mainColor}
-                    className="absolute left-0 -translate-x-1/2 top-0 -translate-y-1/2  z-[998]"
-                  />
-                </div>
-                <div className=""></div>
-                <div className=""></div>
-                <div className="relative border-t-4 border-r-4 border-dashed border-gray-300  rounded-tr-[100%]">
-                  <LessonNodeOnRoadmap
-                    icon={topic.lessons[2].icon}
-                    title={topic.lessons[2].title}
-                    lessonLink={topic.lessons[2].lessonLink}
-                    miniGameLink={topic.lessons[2].miniGameLink}
-                    mainColor={topic.mainColor}
-                    className="absolute left-0 -translate-x-1/2 top-0 -translate-y-1/2 z-[997]"
-                  />
-                </div>
-                <div className=""></div>
-                <div className="relative border-b-4 border-r-4 border-dashed border-gray-300  rounded-br-[100%]">
-                  <LessonNodeOnRoadmap
-                    icon={topic.lessons[3].icon}
-                    title={topic.lessons[3].title}
-                    lessonLink={topic.lessons[3].lessonLink}
-                    miniGameLink={topic.lessons[3].miniGameLink}
-                    mainColor={topic.mainColor}
-                    className="absolute left-full -translate-x-1/2  top-0 -translate-y-1/2 z-[996]"
-                  />
-                  <LessonNodeOnRoadmap
-                    icon={topic.lessons[4].icon}
-                    title={topic.lessons[4].title}
-                    lessonLink={topic.lessons[4].lessonLink}
-                    miniGameLink={topic.lessons[4].miniGameLink}
-                    mainColor={topic.mainColor}
-                    className="absolute right-0 -translate-x-1/2  bottom-0 translate-y-1/2 z-[995]"
-                  />
-                </div>
-              </div>
+              )}
+
               {/* anh minh hoa */}
               {topic.images && topic.images.length > 0 && (
                 <img
