11097f241fd8cab5b8ccc1a54e9df3ff596e93cc
Mai Nguyen Duy Khanh <mndkhanh@gmail.com>
2025-11-16 01:39:04 +0700

    fix navbar navigate falsy

--- src/shared/layout/NavBar.tsx
+++ src/shared/layout/NavBar.tsx
@@ -89,22 +89,22 @@ const NavBar = () => {
               <Link to={item.href} className="flex items-center gap-1">
                 <img src={item.icon} alt="" className="w-[20px] h-[20px]" />
                 <span className="text-sm font-semibold">{item.name}</span>
-                {item.hasDropdown && (
-                  <>
-                    <MdArrowDropDown />
-                    <ul className="absolute top-full left-1/2 -translate-x-1/2  bg-white border w-max border-gray-300 rounded-md shadow-lg opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all mt-2">
-                      {item.dropDownItems!.map((subItem) => (
-                        <li
-                          key={subItem.name}
-                          className="px-5 py-2 hover:bg-gray-100 text-sm font-semibold hover:scale-95 hover:text-[var(--dark-pink)]"
-                        >
-                          <Link to={subItem.href}>{subItem.name}</Link>
-                        </li>
-                      ))}
-                    </ul>
-                  </>
-                )}
+                {item.hasDropdown && <MdArrowDropDown />}
               </Link>
+              {item.hasDropdown && (
+                <>
+                  <ul className="absolute top-full left-1/2 -translate-x-1/2  bg-white border w-max border-gray-300 rounded-md shadow-lg opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all">
+                    {item.dropDownItems!.map((subItem) => (
+                      <li
+                        key={subItem.name}
+                        className="px-5 py-2 hover:bg-gray-100 text-sm font-semibold hover:scale-95 hover:text-[var(--dark-pink)]"
+                      >
+                        <Link to={subItem.href}>{subItem.name}</Link>
+                      </li>
+                    ))}
+                  </ul>
+                </>
+              )}
             </li>
           ))}
           <li>
