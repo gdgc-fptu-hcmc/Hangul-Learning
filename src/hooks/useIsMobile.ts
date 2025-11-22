9da69776b1b04b935c3d2a45f03dd8e1277f6f71
Mai Nguyen Duy Khanh <mndkhanh@gmail.com>
2025-11-22 15:14:44 +0700

    add responsive on topics page

---
+++ src/hooks/useIsMobile.ts
@@ -0,0 +1,15 @@
+import React from "react";
+
+const useIsMobile = () => {
+  const [isMobile, setIsMobile] = React.useState(window.innerWidth < 768);
+
+  React.useEffect(() => {
+    const handleResize = () => setIsMobile(window.innerWidth < 768);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return isMobile;
+};
+
+export default useIsMobile;
